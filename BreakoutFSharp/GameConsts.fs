87ac74e44709bb6885d22b24a6e656a2c756046d
Ben Samuels <bsamuels453@gmail.com>
2014-04-02 18:40:36 -0700

    upping the speeds of everything

--- BreakoutFSharp/GameConsts.fs
+++ BreakoutFSharp/GameConsts.fs
@@ -8,12 +8,12 @@ module GameConsts =
     let screenWidth = 800.0f
     let screenHeight = 600.0f
     let paddleXAxis = 50.0f
-    let numBlockCols = 20.0f
+    let numBlockCols = 14.0f
     let numBlockRows = 6.0f
     let blockHeight = 0.23f * screenHeight/numBlockRows
     let blockWidth = screenWidth/numBlockCols
-    let paddleSpeed = 0.20f
-    let initlBallSpeed = 0.12f
+    let paddleSpeed = 3.5f
+    let initlBallSpeed = 2.80f
 
     let blockXCoords = [for x in 0..(int numBlockCols-1) -> float32 x * blockWidth]
     let blockYCoords = [for y in 0..(int numBlockRows-1) -> float32 y * blockHeight]

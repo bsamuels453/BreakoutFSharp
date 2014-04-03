f75aae54d0efb34cd6be84363a987dacd330b4f6
Ben Samuels <bsamuels453@gmail.com>
2014-04-02 21:00:24 -0700

    removing exclusive spriteid gen funcs

--- BreakoutFSharp/GameFunctions.fs
+++ BreakoutFSharp/GameFunctions.fs
@@ -2,23 +2,9 @@
 
 [<AutoOpen>]
 module GameFunctions =
-    let generatePaddleId = 
+    let generateSpriteId = 
         let count = ref 0;
         (fun () -> 
             count := !count + 1
-            PaddleId(!count)
-            )
-
-    let generateBallId =
-        let count = ref 0;
-        (fun () -> 
-            count := !count + 1
-            BallId(!count)
-            )
-
-    let generateBlockId =
-        let count = ref 0;
-        (fun () -> 
-            count := !count + 1
-            BlockId(!count)
+            !count
             )

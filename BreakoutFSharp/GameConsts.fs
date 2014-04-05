8fadc85402385a91e3bddc3d62f0c4ee5e48a980
Ben Samuels <bsamuels453@gmail.com>
2014-04-05 13:43:33 -0700

    increasitn ball start speed

--- BreakoutFSharp/GameConsts.fs
+++ BreakoutFSharp/GameConsts.fs
@@ -13,7 +13,7 @@ module GameConsts =
     let blockHeight = 0.23f * screenHeight/numBlockRows
     let blockWidth = screenWidth/numBlockCols
     let paddleSpeed = 5.5f
-    let initlBallSpeed = 2.80f
+    let initlBallSpeed = 3.0f
 
     let blockXCoords = [for x in 0..(int numBlockCols-1) -> float32 x * blockWidth]
     let blockYCoords = [for y in 0..(int numBlockRows-1) -> float32 y * blockHeight]
